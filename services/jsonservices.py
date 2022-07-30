8e484fc805a7f3639c9793d7a0aabca6d7a2a655
Julian Bajo <j.bajo@outlook.com>
2022-07-30 13:05:41 +0200

    Implementado el servicio para deserializar un json a una lista

--- services/jsonservices.py
+++ services/jsonservices.py
@@ -3,10 +3,11 @@ import json
 from modelos.modelable import Modelable
 
 
-def json_serializer(filename: str, data_list: list[Modelable]):
+def json_serialize(filename: str, data_list: list[Modelable]):
     with open(filename, 'w') as file:
         json.dump([item.json_serialize() for item in data_list], file)
 
 
-def json_unserializer(filename: str) -> list:
-    pass
+def json_deserialize(filename: str) -> list:
+    with open(filename, 'r') as file:
+        return json.load(file)
