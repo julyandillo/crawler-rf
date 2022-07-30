b482fa9d07656c32d473baeb62ba714e31f42ca8
Julian Bajo <j.bajo@outlook.com>
2022-07-30 13:06:35 +0200

    Usuando el servicio para deserializar jsons en el almacen de equipos

--- almacenes/almacenequipos.py
+++ almacenes/almacenequipos.py
@@ -1,35 +1,38 @@
 import json
 
 from modelos.equipo import Equipo
-from services.jsonservices import json_serializer
+from services.jsonservices import json_serialize, json_deserialize
 
 FILE_TEAMS = 'equipos.json'
+
+
+def _dict_generate_from_list(teams):
+    return {team.get('Nombre corto'): team for team in teams}
 
 
 class AlmacenEquipos:
     def __init__(self):
         self._equipos_almacenados = {}
 
-    def save_all_teams(self, equipos: list[Equipo]):
-        json_serializer(FILE_TEAMS, equipos)
+    def save_all_teams(self, teams: list[Equipo]):
+        json_serialize(FILE_TEAMS, teams)
+        self._equipos_almacenados = _dict_generate_from_list(teams)
 
     def save_team(self, equipo: Equipo):
         if not self._equipos_almacenados:
             self.load_all_teams()
-            
+
         # se añade el nuevo equipo al diccionario (si ya existía se reeemplazará por el nuevo)
         self._equipos_almacenados['Nombre corto'] = equipo
-            
+
         self.save_all_teams(list(self._equipos_almacenados.values()))
 
     def load_team(self, clave: int | str):
         if not self._equipos_almacenados:
             """ los diccionarios vacios se evaluan como False """
             self.load_all_teams()
-            
+
         return self._equipos_almacenados.get(clave, 'Equipo no encontrado')
-        
+
     def load_all_teams(self):
-        with open(FILE_TEAMS, 'r') as fichero:
-            for equipo in json.load(fichero):
-                self._equipos_almacenados[equipo.get('Nombre corto')] = equipo
+        self._equipos_almacenados = _dict_generate_from_list(json_deserialize(FILE_TEAMS))
