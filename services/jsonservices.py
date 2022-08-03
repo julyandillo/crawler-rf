363ba70389b1dbe273b8ee8af97222705c0125c2
Julian Bajo <j.bajo@outlook.com>
2022-08-03 21:25:12 +0200

    Modificando el servicio que genera los json para que tenga identaciones y con caracteres utf-8 en lugar de usar ASCII

--- services/jsonservices.py
+++ services/jsonservices.py
@@ -5,7 +5,7 @@ from modelos.modelable import Modelable
 
 def json_serialize(filename: str, data_list: list[Modelable]):
     with open(filename, 'w') as file:
-        json.dump([item.json_serialize() for item in data_list], file)
+        json.dump([item.json_serialize() for item in data_list], file, indent=2, ensure_ascii=False)
 
 
 def json_deserialize(filename: str) -> list:
