ebf837149d84e47aaa05675e2244e7db1687cd78
Julian Bajo <j.bajo@outlook.com>
2022-08-03 22:09:07 +0200

    Funcion para formatear un fecha con el mismo formato agregada

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
 from modelos.modelable import Modelable
 
 
 def generate_dict_teams_from_list(key: str, teams: list[Modelable]):
     return {team.get(key): team for team in teams}
+
+
+def date_format(date: datetime) -> str:
+    return date.strftime('%d-%m-%Y')
