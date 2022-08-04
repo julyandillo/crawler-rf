17cbec3f9be88cc6d0cf86c5b65b6d4f2f1b98a8
Julian Bajo <j.bajo@outlook.com>
2022-08-04 19:05:25 +0200

    Renombrando los servicios json

--- services/jsonservices.py
+++ services/jsonservices.py
@@ -3,11 +3,11 @@ import json
 from modelos.modelable import Modelable
 
 
-def json_serialize(filename: str, data_list: list[Modelable]):
+def save_as_json(filename: str, data_list: list[Modelable]):
     with open(filename, 'w') as file:
         json.dump([item.json_serialize() for item in data_list], file, indent=2, ensure_ascii=False)
 
 
-def json_deserialize(filename: str) -> list:
+def load_from_json(filename: str) -> list:
     with open(filename, 'r') as file:
         return json.load(file)
