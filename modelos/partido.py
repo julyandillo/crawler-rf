9ddacbee9a4b89b59a44a9f64ed5a994f0984881
Julian Bajo <j.bajo@outlook.com>
2022-08-07 21:14:06 +0200

    Arreglado un problema con nombres de algunos equipos. Se crea un diccionario de reemplazos para guardar el nombre correcto

--- modelos/partido.py
+++ modelos/partido.py
@@ -15,6 +15,11 @@ class Partido(Modelable):
         'fecha'
     ]
 
+    reemplazos = {
+        'Sociedad': 'Real Sociedad',
+        'Real': 'Valladolid'
+    }
+
     def __init__(self):
         super().__init__(Partido.modelo)
         self.__class__.contador += 1
@@ -25,16 +30,18 @@ class Partido(Modelable):
         self.fecha: datetime = datetime.now()
 
     def __str__(self):
-        return f'[{str(self.id).zfill(3)}] | {self.equipo_local.center(15)} - {self.equipo_visitante.center(15)} ' \
+        return f'[{str(self.get("id").zfill(3))}] | {self.equipo_local.center(15)} - {self.equipo_visitante.center(15)} ' \
                f'[{self.get_fecha()} {self.get_hora()}]'
 
     def set_equipo_local(self, nombre_equipo: str):
-        self.equipo_local = nombre_equipo
-        self.set('equipo_local', nombre_equipo)
+        self.equipo_local = nombre_equipo if nombre_equipo not in self.__class__.reemplazos.keys() else \
+            self.__class__.reemplazos.get(nombre_equipo)
+        self.set('equipo_local', self.equipo_local)
 
     def set_equipo_visitante(self, nombre_equipo: str):
-        self.equipo_visitante = nombre_equipo
-        self.set('equipo_visitante', nombre_equipo)
+        self.equipo_visitante = nombre_equipo if nombre_equipo not in self.__class__.reemplazos.keys() else \
+            self.__class__.reemplazos.get(nombre_equipo)
+        self.set('equipo_visitante', self.equipo_visitante)
 
     def set_fecha(self, fecha: datetime):
         self.fecha = fecha
