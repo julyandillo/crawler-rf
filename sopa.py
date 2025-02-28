23d0097affe735f12a618d8989cc77d5a18b1174
Julian Bajo <j.bajo@outlook.com>
2025-02-28 13:09:03 +0100

    Limpiando la clase

--- sopa.py
+++ sopa.py
@@ -4,18 +4,23 @@ from bs4 import BeautifulSoup
 from exceptions.crawler_error import CrawlerError
 
 
+def get_content_url(url) -> str:
+    response = requests.get(url)
+
+    if response.status_code != requests.codes.ok:
+        raise CrawlerError(f"Ha ocurrido un error al realizar la petición.\n{url} - {response.status_code}")
+
+    response.encoding = 'utf-8'
+    return response.text
+
+
 class Sopa:
     def __init__(self, url: str):
         self._url = url
-        self._response = None
 
-    def get_sopa(self) -> BeautifulSoup:
-        self.realiza_peticion()
-        return BeautifulSoup(self._response.text, 'lxml')
+    def get_sopa(self) -> BeautifulSoup | None:
+        try:
+            return BeautifulSoup(get_content_url(self._url), 'lxml')
 
-    def realiza_peticion(self):
-        self._response = requests.get(self._url)
-        if self._response.status_code != requests.codes.ok:
-            raise CrawlerError('Ha ocurrido un error al realizar la petición. ' + self._url)
-
-        self._response.encoding = 'utf-8'
+        except CrawlerError as e:
+            print(e)
